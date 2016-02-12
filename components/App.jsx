ac4b6f969196b590f8d0077c76d88d80cda4eff7
ricardomarquesramos <ricardomarquesramos@gmail.com>
2016-02-12 10:26:07 +0000

    first commit

---
+++ components/App.jsx
@@ -0,0 +1,17 @@
+import React,  {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div className='app'>
+        <center>EasyWorkShop1 - Introduction to React.</center>
+      </div>
+    );
+  }
+}
+
+export default App;
